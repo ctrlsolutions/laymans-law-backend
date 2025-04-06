a0de62e1779d7572771143bdda2048a8caa0f807
Az <arbusinos@up.edu.ph>
2025-04-07 06:47:09 +0800

    [feat:partial][az] adds backend for submit case

--- case/serializers.py
+++ case/serializers.py
@@ -6,3 +6,8 @@ class CaseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Case
         fields = '__all__'
+
+class CaseSubmitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Case
+        fields = ['title', 'description', 'case_type']  # created_by and status will be handled automatically
