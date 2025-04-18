88ae857505c89ca1e696ba64e3f556bbb2e11f51
Caleb <calebjosh.berandoy@gmail.com>
2025-04-18 23:28:20 +0800

    [feat][caleb] initial commit

--- case/serializers.py
+++ case/serializers.py
@@ -1,8 +1,25 @@
-# case/serializers.py
 from rest_framework import serializers
 from .models import Case
+from django.contrib.auth import get_user_model
+
+from user.serializers import NestedUserSerializer
+
+User = get_user_model()
 
 class CaseSerializer(serializers.ModelSerializer):
+    created_by = NestedUserSerializer(read_only=True)
+    assigned_to = NestedUserSerializer(read_only=True, allow_null=True)
     class Meta:
         model = Case
-        fields = '__all__'
+        fields = (
+            # 'id',
+            'title',
+            'description',
+            'created_by',
+            'assigned_to',
+            'status',
+            'case_type',
+            'created_date',
+            'accepted_date',
+        )
+        read_only_fields = ('created_by', 'assigned_to', 'created_date', 'accepted_date', 'status')
