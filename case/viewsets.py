fc7d5fa6ebcc5053df26a88228c25820ba3c0c66
Caleb <calebjosh.berandoy@gmail.com>
2025-04-19 21:29:27 +0800

    [feat][caleb] imports status in case.viewsets to fix error

--- case/viewsets.py
+++ case/viewsets.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.decorators import action
