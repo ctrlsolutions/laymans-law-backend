8053abb6cf133a06a025881cf6c494aac18754a1
Lanieza <laniezalopez16@gmail.com>
2025-05-14 13:31:18 +0800

    [feat][lanie] ofw support models, serializer, viewsets, and router

--- LaymansLaw/routers.py
+++ LaymansLaw/routers.py
@@ -1,12 +1,13 @@
 from rest_framework import routers
 from user.viewsets import AuthViewSet
 from case.viewsets import CaseViewSet
-from wiki.viewsets import LawViewSet
+from wiki.viewsets import LawViewSet, OFWSupportDetailViewSet
 
 router = routers.SimpleRouter()
 
 router.register(r'user', AuthViewSet, basename="user")
 router.register(r'cases', CaseViewSet, basename="case")
 router.register(r'laws', LawViewSet, basename="law")
+router.register(r'ofw-support', OFWSupportDetailViewSet, basename="ofw")
 
 urlpatterns = router.urls
--- wiki/viewsets.py
+++ wiki/viewsets.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from .models import Law, Summary, Translation
-from .serializers import LawSerializer, SummarySerializer, TranslationSerializer
+from .models import Law, Summary, Translation, OFWSupportDetail
+from .serializers import LawSerializer, SummarySerializer, TranslationSerializer, OFWSupportDetailSerializer
 
 class LawViewSet(viewsets.ModelViewSet):
     queryset = Law.objects.all()
@@ -13,3 +13,7 @@ class SummaryViewSet(viewsets.ModelViewSet):
 class TranslationViewSet(viewsets.ModelViewSet):
     queryset = Translation.objects.all()
     serializer_class = TranslationSerializer
+    
+class OFWSupportDetailViewSet(viewsets.ModelViewSet):
+    queryset = OFWSupportDetail.objects.all()
+    serializer_class = OFWSupportDetailSerializer
