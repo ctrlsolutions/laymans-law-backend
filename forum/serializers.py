302fada014ac5c0585dec50ad1c0d676782ef060
Lanieza <laniezalopez16@gmail.com>
2025-05-22 14:11:17 +0800

    [feat][lanie] final forum backend

--- forum/serializers.py
+++ forum/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from .models import ForumPost
+from .models import ForumPost, Reply, Comment, ForumPostBookmark
 
 User = get_user_model()
 
@@ -9,7 +9,7 @@ class ForumPostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ForumPost
-        fields = ['id', 'author', 'title', 'content', 'timestamp', 'category', 'bookmark']
+        fields = ['id', 'author', 'title', 'content', 'timestamp', 'category']
         read_only_fields = ['author', 'timestamp']
 
     def get_author(self, obj):
@@ -17,3 +17,25 @@ class ForumPostSerializer(serializers.ModelSerializer):
             'first_name': obj.author.first_name,
             'last_name': obj.author.last_name
         }
+
+class ReplySerializer(serializers.ModelSerializer):
+    author_username = serializers.CharField(source='author.username', read_only=True)
+
+    class Meta:
+        model = Reply
+        fields = ['id', 'author', 'author_username', 'content', 'created_at']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author_username = serializers.CharField(source='author.username', read_only=True)
+    replies = ReplySerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'post', 'author', 'author_username', 'content', 'created_at', 'updated_at', 'replies']
+
+class ForumPostBookmarkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ForumPostBookmark
+        fields = ['id', 'user', 'post', 'created_at']
+        read_only_fields = ['user', 'created_at']
