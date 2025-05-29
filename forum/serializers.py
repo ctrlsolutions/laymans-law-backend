3c735eb1364726898aa8e32647e5f4f575e1fc2d
Lanieza <lblopez@up.edu.ph>
2025-05-29 18:19:46 +0800

    [feat][lanie] new forum: removed the comment and reply

--- forum/serializers.py
+++ forum/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from .models import ForumPost, Reply, Comment, ForumPostBookmark
+from .models import ForumPost, ForumPostBookmark
 
 User = get_user_model()
 
@@ -19,22 +19,6 @@ class ForumPostSerializer(serializers.ModelSerializer):
             'first_name': obj.author.first_name,
             'last_name': obj.author.last_name
         }
-
-class ReplySerializer(serializers.ModelSerializer):
-    author_username = serializers.CharField(source='author.username', read_only=True)
-
-    class Meta:
-        model = Reply
-        fields = ['id', 'author', 'author_username', 'content', 'created_at']
-
-
-class CommentSerializer(serializers.ModelSerializer):
-    author_username = serializers.CharField(source='author.username', read_only=True)
-    replies = ReplySerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Comment
-        fields = ['id', 'post', 'author', 'author_username', 'content', 'created_at', 'updated_at', 'replies']
 
 class ForumPostBookmarkSerializer(serializers.ModelSerializer):
     class Meta:
