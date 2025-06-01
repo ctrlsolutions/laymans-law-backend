ef1657e25b86bca0ecde13a3e74fd10af073c7fa
Caleb <calebjosh.berandoy@gmail.com>
2025-06-01 13:40:38 +0800

    {feat][caleb] initial commit

--- case/models.py
+++ case/models.py
@@ -89,3 +89,16 @@ class CaseAttachment(models.Model):
 
     def __str__(self):
         return f"Attachment for Case {self.case.id} - {self.file.name}"
+    
+class Comment(models.Model):
+    case = models.ForeignKey(Case, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_lawyer = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.case.title}"
